47a20396f9c922afb197fb6df1aa4973d3b798c9
ewertoncasado <tom_casado@hotmail.com>
2025-03-10 01:47:07 -0300

    Finalizando o Blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Solo Leveling é o anime mais assistido do momento",
+    "description": "Ultimos episodios da segunda temporada estão disponíveis no Crunchyroll",
+    "photoCover":"https://seliganerd.com/wp-content/uploads/solo_leveling_o_que_e__w24vo9-1.webp"
+  },
+  {
+    "id":"2",
+    "title": "3ª temporada de One Punch é confirmada",
+    "description": " A terceira temporada de One Punch Man ganhou um novo trailer e previsão de estreia em outubro de 2025. ",
+    "photoCover":"https://i2.wp.com/guiadoanime.com.br/wp-content/uploads/2024/01/Por-que-o-Saitama-de-One-Punch-Man-e-tao-forte.jpg"
+  },
+  {
+    "id":"3",
+    "title": "One Piece volta em 6 de abril de 2025.",
+    "description": " A data foi confirmada durante a Jump Festa 2025.",
+    "photoCover":"https://www.acidadeon.com/tudoep/wp-content/uploads/sites/10/2023/12/one_piece_1200x675_07082023172751.jpg"
+  },
+  {
+    "id":"4",
+    "title": "A segunda temporada está prevista para estrear em julho de 2025",
+    "description": "Dan Da Dan é um anime que conta a história de um casal de adolescentes que tenta provar a existência de alienígenas e fantasmas.  ",
+    "photoCover":"https://i.ytimg.com/vi/Nz2dPbRjKTc/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB_qW74Cz6UVqhLGSW_-mT8Fcp8hw"
+  }
+]
